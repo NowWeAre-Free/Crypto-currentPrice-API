0136a19db9313d7f8f8de0cae2ff3ce1d4de8603
NowWeAre-Free <kontodarm@wp.pl>
2023-03-17 11:59:09 +0100

    add api crypto price

--- src/JS/app.js
+++ src/JS/app.js
@@ -5,3 +5,23 @@ burger.addEventListener("click", () => {
     menu.classList.toggle("nav__menu--active");
     burger.classList.toggle("nav__options-burger--active")
 })
+
+//price api
+
+const url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Chex&vs_currencies=usd";
+const btc = document.querySelector(".crypto__btc-price");
+const eth = document.querySelector(".crypto__eth-price");
+const hex = document.querySelector(".crypto__hex-price");
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+
+    const bitcoinPrice = data["bitcoin"]["usd"];
+    const ethereumPrice = data["ethereum"]["usd"];
+    const hexPrice = data["hex"]["usd"];
+    btc.innerText = `$ ${bitcoinPrice}`;
+    eth.innerText = `$ ${ethereumPrice}`;
+    hex.innerText = `$ ${hexPrice}`;
+  })
+  .catch(error => alert(error));
